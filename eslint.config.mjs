bee9764e4829b3cd93b4e3ca8b54743798320698
Ivan Tolmachev <ivatolm@gmail.com>
2024-08-23 18:11:10 +0300

    Update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,13 @@ export default [
   ...tseslint.configs.recommended,
   stylistic.configs['recommended-flat'],
   {
-    files: ['**/*.{js,mjs,cjs,ts}']
+    files: ['**/*.{js,mjs,cjs,ts}'],
   },
   {
     languageOptions: { globals: globals.node },
     rules: {
       '@stylistic/semi': 'error',
-      '@stylistic/comma-dangle': ['error', 'always']
-    }
-  }
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    },
+  },
 ]
