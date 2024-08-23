84fc51bacd0f71ce9949ee5aee05635b4fc2c513
Ivan Tolmachev <ivatolm@gmail.com>
2024-08-23 17:51:58 +0300

    Setup linters and CI

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs['recommended-flat'],
+  {
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      '@stylistic/semi': 'error',
+      '@stylistic/comma-dangle': ['error', 'always']
+    }
+  }
+]
